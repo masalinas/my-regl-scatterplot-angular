bc06f080c76d6d28b91f6224c3112e5e274b1e04
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2023-11-01 16:55:27 +0100

    last commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/my-regl-scatterplot-angular'),
+      dir: require('path').join(__dirname, './coverage/poc-regl-scatterplot-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
